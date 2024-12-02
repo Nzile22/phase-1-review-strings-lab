ac22be75e3c6a8ff66acc6ad3286150d4fc55aaf
Nzile22 <diananduku00@gmail.com>
2024-12-02 16:53:23 +0300

    strings-lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code in this file!
+const currentUser = 'Diana Nzile';
+
+// const welcomeMessage = 'Welcome to Flatbook, '
+
+// const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// const shortGreeting = 'Welcome, ';
+
+// const shortGreeting = `Welcome, ${currentUser}`;
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
